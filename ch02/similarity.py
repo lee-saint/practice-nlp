bbcba0e6376c2b9213ab9f66b087149968aa7069
Sung-in Lee <plutorian131@gmail.com>
2020-01-06 22:33:52 +0900

    200116 공부
    Ch02 끝냄

--- ch02/similarity.py
+++ ch02/similarity.py
@@ -37,6 +37,25 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
             return
 
 
+def ppmi(C, verbose=False, eps=1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total//100) == 0:
+                    print('%.1f%% 완료' % (100*cnt / total))
+    return M
+
+
 if __name__ == '__main__':
     text = 'You say goodbye and I say hello.'
     corpus, word_to_id, id_to_word = preprocess(text)
@@ -48,3 +67,12 @@ if __name__ == '__main__':
     print(cos_similarity(c0, c1))
 
     most_similar('you', word_to_id, id_to_word, C, top=5)
+
+    W = ppmi(C)
+    
+    np.set_printoptions(precision=3)  # 유효 자릿수를 세 자리로 표시
+    print('동시발생 행렬')
+    print(C)
+    print('-'*50)
+    print('PPMI')
+    print(W)
